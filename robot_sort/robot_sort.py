92caad83722e935ea80a7f6c259af4e801152035
TJ-Feledy <teej387@yahoo.com>
2020-03-08 22:29:09 -0500

    Reformatted robot_sort

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -141,10 +141,10 @@ class SortingRobot:
                 if self.compare_item() == 1:
                     self.set_light_on()
                     self.swap_item()
+                    self.move_left()
+                    self.swap_item()
+                    self.move_right()
                     
-                self.move_left()
-                self.swap_item()
-                self.move_right()
 
             if self.light_is_on() == False:
                     return
@@ -163,10 +163,10 @@ class SortingRobot:
                 if self.compare_item() == -1:
                     self.set_light_on()
                     self.swap_item()
+                    self.move_right()
+                    self.swap_item()
+                    self.move_left()
 
-                self.move_right()
-                self.swap_item()
-                self.move_left()
 
             if self.light_is_on() == False:
                 return
